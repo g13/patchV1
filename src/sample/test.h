94924fa02f8f42989f4ffdea98b923fd5fcb6279
wei <gueux13@gmail.com>
2018-11-08 13:57:45 -0500

    l2s

--- src/sample/test.h
+++ src/sample/test.h
@@ -5,7 +5,3 @@
 #include <cmath>
 #include "cpu.h"
 
-extern __constant__ double vE[1], vI[1], vT[1];
-extern __constant__ double gL_E[1], gL_I[1];
-extern __constant__ double tRef_E[1], tRef_I[1];
-extern __constant__ unsigned int nE[1];
