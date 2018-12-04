6163eb3b0a7248d097aa3c230126e66a20c4dec5
wei <gueux13@gmail.com>
2018-12-04 16:54:53 -0500

    misc_l

---
+++ src/CONST.h
@@ -0,0 +1,13 @@
+#ifndef CONST_H
+#define CONST_H
+
+#define vE 14.0f/3.0f
+#define vI -2.0f/3.0f
+#define vT 1.0f
+#define vL 0.0f
+#define gL_E 0.05f
+#define gL_I 0.1f
+#define tRef_E 0.5f
+#define tRef_I 0.25f
+
+#endif
