cf6b8e4046ac77d3f43307f774f0068db4bfda67
wei <gueux13@gmail.com>
2019-07-19 07:13:14 -0400

    loop correction

--- parameters.m
+++ parameters.m
@@ -22,51 +22,6 @@ non_cortical_lr = false;
 cortical_shape = false;
 uniform_LR = true;
 % uniform_LR = false;
-if ~cortical_shape
-%     test_dw = 15;
-%     test_dh = 20;
-    test_dw = 5;
-    test_dh = 7;
-end
-copyfile('parameters.m',[ENfilename0,'_p.m']);
-% Objective function weights
-
-alpha = 1;			% Fitness term weight
-beta = 100;  		% Tension term weight
-% Training parameters
-iters = 10;%21;			% No. of annealing iterations (saved)
-max_it = 10;%12;			% No. of annealing iterations (not saved) ***
-Kin = 0.14;			% Initial K ***
-Kend = 0.02;        % Final K ***    
-% - VFx: Nx points in [0,1], with interpoint separation dx.
-Nx = 8;			% Number of points along VFx ***
-nvf = 4;
-rx = [0 0.16]*nvf;			% Range of VFx
-dx = diff(rx)/(Nx-1);		% Separation between points along VFx
-% - VFy: ditto for Ny, dy.
-Ny = 13;			% Number of points along VFy ***
-ry = [0 0.26]*nvf;			% Range of VFy
-dy = diff(ry)/(Ny-1);		% Separation between points along VFy    
-d = (dx + dy)/2;
-% - OD: NOD values in range rOD, with interpoint separation dOD.
-l = 0.12;			% Half-separation between OD layers ***
-NOD = 2;			% Number of points along OD
-rOD = [-l l];			% Range of OD
-dOD = diff(rOD)/(NOD-1);	% Separation between points along OD
-%  coded as NOR Cartesian-coordinate pairs (ORx,ORy). -- later by pol2cart
-%  r = 6*l/pi
-r = 1.0*l;			% OR modulus -- the radius of pinwheel
-NOR = 8;	 %8;		% Number of points along OR    
-% for myCortex patch
-ODnoise = l*0.0;
-ODabsol = 1.0;
-nG = 2;
-G = [64 104]*nG;		% Number of centroids *** 
-ecc = 2;
-nod = 25;
-a = 0.635; b = 96.7; k = sqrt(140)*0.873145;
-fign = 106;
-
 plots = false;
 new = false;
 range = [2,4,6,8,10];
@@ -79,9 +34,53 @@ if ~isempty(poolobj)
         parpool(length(range));
     end
 end
+copyfile('parameters.m',[ENfilename0,'_p.m']);
 parfor i = 1:length(range)
+    if ~cortical_shape
+    %     test_dw = 15;
+    %     test_dh = 20;
+        test_dw = 5;
+        test_dh = 7;
+    end
+    % Objective function weights
+
+    alpha = 1;			% Fitness term weight
+    beta = 100;  		% Tension term weight
+    % Training parameters
+    iters = 10;%21;			% No. of annealing iterations (saved)
+    max_it = 10;%12;			% No. of annealing iterations (not saved) ***
+    Kin = 0.14;			% Initial K ***
+    Kend = 0.02;        % Final K ***    
+    % - VFx: Nx points in [0,1], with interpoint separation dx.
+    Nx = 8;			% Number of points along VFx ***
     nvf = range(i);
-    ENfilename = [ENfilename0,'-',var,'-',num2str(nvf)];
+%     nvf = 4;
+    rx = [0 0.16]*nvf;			% Range of VFx
+    dx = diff(rx)/(Nx-1);		% Separation between points along VFx
+    % - VFy: ditto for Ny, dy.
+    Ny = 13;			% Number of points along VFy ***
+    ry = [0 0.26]*nvf;			% Range of VFy
+    dy = diff(ry)/(Ny-1);		% Separation between points along VFy    
+    d = (dx + dy)/2;
+    % - OD: NOD values in range rOD, with interpoint separation dOD.
+    l = 0.12;			% Half-separation between OD layers ***
+    NOD = 2;			% Number of points along OD
+    rOD = [-l l];			% Range of OD
+    dOD = diff(rOD)/(NOD-1);	% Separation between points along OD
+    %  coded as NOR Cartesian-coordinate pairs (ORx,ORy). -- later by pol2cart
+    %  r = 6*l/pi
+    r = 1.0*l;			% OR modulus -- the radius of pinwheel
+    NOR = 8;	 %8;		% Number of points along OR    
+    % for myCortex patch
+    ODnoise = l*0.0;
+    ODabsol = 1.0;
+    nG = 2;
+    G = [64 104]*nG;		% Number of centroids *** 
+    ecc = 2;
+    nod = 25;
+    a = 0.635; b = 96.7; k = sqrt(140)*0.873145;
+    fign = 106;
+    ENfilename = [ENfilename0,'-',var,'-',num2str(range(i))];
     stats(i) = myV1driver(seed,ENproc,ENfilename,non_cortical_lr,cortical_shape,uniform_LR,test_dw,test_dh,alpha,beta,iters,max_it,Kin,Kend,Nx,nvf,rx,dx,Ny,ry,dy,l,NOD,rOD,dOD,r,NOR,ODnoise,ODabsol,nG,G,ecc,nod,a,b,k,fign,plots,new);
 end
 nnpinw = [stats.npinw];
