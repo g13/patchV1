9ab40f8c63f7df19e1370ab5a391e922703cd9c6
wei <gueux13@gmail.com>
2020-05-09 23:53:51 -0400

    ext input

--- grating.py
+++ grating.py
@@ -3,7 +3,86 @@ import cv2 as cv
 import functools
 from ext_signal import *
 #TODO: heterogeneous buffers, to save texture memory
-def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel, c1, c2, fname, time, phase, sharpness, frameRate = 120, ecc = 2.5, buffer_ecc = 0.25, gtype = 'drifting', neye = 2):
+def generate_random(amp, cSize, c0, fname, time, frameRate = 120, ecc = 2.5, buffer_ecc = 0.25, neye = 2, gtype = 'uniform', seed = None, shift = None):
+    """
+    buffer_ecc: buffering area, to avoid border problems in texture memory accesses
+    neye == 2:
+        frame from 2 visual fields: (ecc+2*buffer_ecc) x 2(ecc+buffer_ecc+buffer_ecc(unused)) (width x height)
+        each has a temporal axis: [-buffer_ecc, ecc], and vertical axis [-ecc-buffer_ecc, ecc+buffer_ecc] in degree
+    neye == 1:
+        frame from a single visual fields: origin at the center 2(ecc+buffer_ecc) x 2(ecc+buffer_ecc) (width x height)
+    """
+    if neye == 1:
+        npixel = int(round(2*(ecc+buffer_ecc)/cSize))
+        a = npixel
+    else:
+        npixel = int(round(2*(ecc+2*buffer_ecc)/cSize))
+        a = npixel//2
+    b = npixel  
+    FourCC = cv.VideoWriter_fourcc(*'HFYU')
+    output = cv.VideoWriter(fname+'.avi', FourCC, frameRate, (npixel,npixel), True)
+
+    if isinstance(time, (list, tuple, np.ndarray)):
+        nseq = len(time)
+    else:
+        nseq = 1
+        time = np.array([time])
+
+    if isinstance(amp, (list, tuple, np.ndarray)):
+        assert(len(amp) == nseq)
+    else:
+        amp = np.zeros(nseq) + amp
+
+    ########### VIDEO encodes as BGR: 
+    # rgb->bgr
+    c0 = np.reshape(c0[::-1],(1,3))
+
+    np.random.seed(seed)
+    LMS = np.empty((nseq,), dtype=object)
+    for i in range(nseq):
+        t = time[i]
+        nstep = np.round(frameRate * t)
+        if not nstep == frameRate*t:
+            raise Exception(f'time duration of sequence {i} is not in multiples of frame duration')
+        nstep = int(nstep)
+        if np.mod(nstep,2) != 0 and gtype == 'rotating':
+            raise Exception(f'need even time step, current: {nstep}')
+        print(f'{nstep} frames in total')
+
+        LMS_seq = np.empty((nstep,)+(3, npixel, npixel), dtype=float)
+        for it in range(nstep):
+            if neye == 1:
+                data = randomized(amp[i], a, b, c0, gtype)
+
+            else:
+                dataL = randomized(amp[i], a, b, c0, gtype)
+                if shift is not None:
+                    assert(shift == 0)
+                    # TODO: implement nonzero shift 
+                    dataR = dataL
+                else:
+                    dataR = randomized(amp[i], a, b, c0, gtype)
+
+                data = np.concatenate((dataL,dataR), axis = 1)
+
+            pixelData = np.reshape(np.round(data*255), (npixel,npixel,3)).astype('uint8')
+            # bgr->lms
+            LMS_seq[it,:,:,:] = data[:,:,::-1].reshape((npixel*npixel,3)).T.reshape((3,npixel,npixel))
+
+            output.write(pixelData)
+            #pixelData = np.reshape(np.round(data*255), (b,a,3))
+            #cv.imshow('linear', pixelData)
+            #cv.waitKey(0)
+            #pixelData = adjust_gamma(pixelData, gamma = 2.2)
+            #cv.imshow('gamma', pixelData)
+            #cv.waitKey(0)
+        LMS[i] = LMS_seq.copy()
+
+    output.release()
+    cv.destroyAllWindows()
+    return LMS
+
+def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel, c1, c2, fname, time, phase, sharpness, frameRate = 120, ecc = 2.5, buffer_ecc = 0.25, gtype = 'drifting', neye = 2, bar = False, center = np.pi/2, wing = np.pi/2):
     """
     spatialFrequency: cycle per degree
     temporalFrequency: Hz
@@ -80,21 +159,23 @@ def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel
     LMS = np.empty((nseq,), dtype=object)
     for i in range(nseq):
         t = time[i]
-        nstep = np.round(frameRate * t)
+        nstep = int(np.round(frameRate*t))
         if not nstep == frameRate*t:
-            raise Exception(f'time duration of sequence {i} is not in multiples of frame duration')
-        nstep = int(nstep)
+            nstep = int(np.round(frameRate*t))
+            print(f'adjusted to {nstep} frames in total')
+        else:
+            print(f'exact {nstep} frames in total')
+
         if np.mod(nstep,2) != 0 and gtype == 'rotating':
             raise Exception(f'need even time step, current: {nstep}')
-        print(f'{nstep} frames in total')
 
         radTF = temporalFrequency[i]*2*np.pi
         radSF = spatialFrequency[i]*180/np.pi*2*np.pi
         s = sharpness[i]
         print(f'sharpness={s}')
         @logistic(s)
-        def grating(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y):
-            return sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y)
+        def grating(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y, bar, center = 0, wing = 0):
+            return sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y, bar, center, wing)
 
         if gtype == 'rotating':
             half = nstep//2 
@@ -112,17 +193,17 @@ def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel
             t = it * dt
             if neye == 1:
                 if gtype == 'rotating':
-                    data = grating(amp[i], radTF, radSF, direction[i]-dd[it], a, b, c1, c2, phase[i], t, X, Y)
+                    data = grating(amp[i], radTF, radSF, direction[i]-dd[it], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
                 if gtype == 'drifting':
-                    data = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y)
+                    data = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
 
             else:
                 if gtype == 'rotating':
-                    dataL = grating(amp[i], radTF, radSF, direction[i]-dd[it], a, b, c1, c2, phase[i], t, X, Y)
-                    dataR = grating(amp[i], radTF, radSF, direction[i]+dd[it], a, b, c1, c2, phase[i], t, X, Y)
+                    dataL = grating(amp[i], radTF, radSF, direction[i]-dd[it], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
+                    dataR = grating(amp[i], radTF, radSF, direction[i]+dd[it], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
                 if gtype == 'drifting':
-                    dataL = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y)
-                    dataR = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y)
+                    dataL = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
+                    dataR = grating(amp[i], radTF, radSF, direction[i], a, b, c1, c2, phase[i], t, X, Y, bar, center, wing)
                 data = np.concatenate((dataL,dataR), axis = 1)
 
             pixelData = np.reshape(np.round(data*255), (npixel,npixel,3)).astype('uint8')
@@ -138,72 +219,6 @@ def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel
             #cv.waitKey(0)
         LMS[i] = LMS_seq.copy()
 
-    output.release()
-    cv.destroyAllWindows()
-    return LMS
-
-def generate_cyclop(amp, spatialFrequency, temporalFrequency, direction, npixel, c1, c2, fname, time = 1, phase = 0, sharpness = 0, frameRate = 120, ecc = 2.5, buffer_ecc = 0.25, gtype = 'drifting'):
-    """
-    spatialFrequency: cycle per degree
-    temporalFrequency: Hz
-    direction: 0-2pi in rad
-    phase: 0-2pi in rad
-    a: width of the half image in pixels 
-    b: height of the image in pixels 
-    c1, c2: the two opposite color in rgb values
-    sharpness:  y = A/(1+exp(-sharpness*(x-0.5)) + C, y=x when sharpness = 0
-    buffer_ecc: buffering area, to avoid border problems in texture memory accesses
-    frame from a single visual fields: origin at the center 2(ecc+buffer_ecc) x 2(ecc+buffer_ecc) (width x height)
-    """
-    nstep = np.round(frameRate * time)
-    if np.mod(nstep,2) != 0 and gtype == 'rotating':
-        raise Exception(f'need even time step, current: {nstep}')
-
-    a = npixel
-    b = npixel  
-    FourCC = cv.VideoWriter_fourcc(*'HFYU')
-    output = cv.VideoWriter(fname+'.avi', FourCC, frameRate, (npixel,npixel), True)
-    print(f'{nstep} frames in total')
-    radTF = temporalFrequency*2*np.pi
-    radSF = spatialFrequency*180/np.pi*2*np.pi
-    ########### VIDEO encodes as BGR: 
-    # rgb->bgr
-    c1 = np.reshape(c1[::-1],(1,3))
-    c2 = np.reshape(c2[::-1],(1,3))
-
-    X, Y = meshgrid(np.linspace(-1,1,a)*(ecc+buffer_ecc)*np.pi/180, np.linspace(-1,1,b)*(ecc+buffer_ecc)*np.pi/180)
-
-    print(f'sharpness={sharpness}')
-    @logistic(sharpness)
-    def grating(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y):
-        return sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y)
-
-    half = nstep//2 
-    dd = np.linspace(0,np.pi/4,half)
-        
-    dt = 1.0/frameRate
-    #for it in range(1):
-    if gtype not in ('drifting','rotating'):
-        raise Exception(f'gtype {gtype} not implemented')
-    LMS = np.empty((nstep,)+(3, b, a), dtype=float)
-    for it in range(nstep):
-        t = it * dt
-        if gtype == 'rotating':
-            data = grating(amp, radTF, radSF, direction-dd[it], a, b, c1, c2, phase, t, X, Y)
-        if gtype == 'drifting':
-            data = grating(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y)
-
-        pixelData = np.reshape(np.round(data*255), (b,a,3)).astype('uint8')
-        # bgr->lms
-        LMS[it,:,:,:] = data[:,:,::-1].reshape((b*a,3)).T.reshape((3,b,a))
-        #pixelData = np.reshape(np.round(data*255), (b,a,3))
-        #cv.imshow('linear', pixelData)
-        #cv.waitKey(0)
-        #pixelData = adjust_gamma(pixelData, gamma = 2.2)
-        #cv.imshow('gamma', pixelData)
-        #cv.waitKey(0)
-        output.write(pixelData)
-    
     output.release()
     cv.destroyAllWindows()
     return LMS
@@ -232,9 +247,29 @@ def logistic(sharpness):
         return static_nolinearity
     return decorator_logistic
 
-def sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y):
-    rel_color = np.reshape(1+amp*np.sin((np.cos(direction)*X + np.sin(direction)*Y)*radSF - radTF*t + phase), (a*b,1))/2
+def sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, phase, t, X, Y, bar, center, wing):
+    phi = (np.cos(direction)*X + np.sin(direction)*Y)*radSF - radTF*t
+    if bar:
+        #floor_phi = np.floor(phi/(2*np.pi)).astype(int)
+        #phi_tmp = phi-floor_phi*2*np.pi
+        pick = np.abs(phi + (phase + center)) > wing
+        phi[pick] = -phase
+    rel_color = np.reshape(1+amp*np.sin(phi + phase), (a*b,1))/2
     color = np.matmul(np.ones((a*b,1)), c1) + np.matmul(rel_color, (c2-c1))
+    return color.reshape((b,a,3))/255
+
+
+def randomized(amp, a, b, c0, gtype):
+    if gtype == 'uniform':
+        rel_color = 1+amp*np.random.uniform(size = (a*b,3))
+    else:
+        if gtype == 'normal':
+            rel_color = 1+amp*np.random.randn(a*b,3)
+        else:
+            raise Exception(f'gtype: {gtype} not implemented')
+    color = rel_color*c0
+    color[color > 255] = 255
+    color[color < 0] = 0
     return color.reshape((b,a,3))/255
 
 def meshgrid(x,y):
