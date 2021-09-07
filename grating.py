667c47baf99aa67733f7ad3cae7c2cf5eda5811b
g13 <gueux13@gmail.com>
2021-09-07 22:42:07 +0800

    sav before trans, ext_input

--- grating.py
+++ grating.py
@@ -24,7 +24,7 @@ def generate_random(amp, radius, npixel, c, fname, time, frameRate = 120, ecc = 
         if np.mod(npixel,2) != 0:
             print('failed: npixel need to be even for neye == 2')
             return
-
+    print(f'{1/deg2pixel} degree per pixel')
     radius_in_pixel = max(int(round(radius*deg2pixel)),1)
     if radius_in_pixel == 1:
         print('radius smaller than one pixel, mode changed to pixel')
@@ -48,6 +48,7 @@ def generate_random(amp, radius, npixel, c, fname, time, frameRate = 120, ecc = 
 
     print(f'ecc = {ecc}, buffer_ecc = {buffer_ecc}')
     f = open(fname + '.bin', 'wb')
+    np.array([-1]).astype('i4').tofile(f) 
     nFrame = int(np.round(frameRate*np.sum(time)))
     np.array([nFrame, npixel, npixel], dtype='i4').tofile(f)
     mean_value = np.mean(c, axis = 0)
@@ -64,7 +65,6 @@ def generate_random(amp, radius, npixel, c, fname, time, frameRate = 120, ecc = 
         nstep = int(nstep)
         print(f'{nstep} frames in total')
 
-        LMS_seq = np.empty((3, npixel, npixel), dtype=float)
         for it in range(nstep):
             if neye == 1:
                 if gtype == 'pixel':
@@ -134,21 +134,21 @@ def generate_random(amp, radius, npixel, c, fname, time, frameRate = 120, ecc = 
 
 def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel, c1, c2, fname, time, phase, sharpness, frameRate = 120, ecc = 2.5, buffer_ecc = 0.25, gtype = 'drifting', neye = 2, bar = False, center = np.pi/2, wing = np.pi/2, mask = None, maskData = None, inputLMS = False, genMovie = True):
     """
-    spatialFrequency: cycle per degree
-    temporalFrequency: Hz
-    direction: 0-2pi in rad
-    phase: 0-2pi in rad
-    a: width of the half image in pixels 
-    b: height of the image in pixels 
-    c1, c2: the two opposite color in rgb values
-    sharpness:  y = A/(1+exp(-sharpness*(x-0.5)) + C, y=x when sharpness = 0
-    buffer_ecc: buffering area, to avoid border problems in texture memory accesses
-    neye == 2:
-        frame from 2 visual fields: (ecc+2*buffer_ecc) x 2(ecc+buffer_ecc+buffer_ecc(unused)) (width x height)
-        each has a temporal axis: [-buffer_ecc, ecc], and vertical axis [-ecc-buffer_ecc, ecc+buffer_ecc] in degree
-    neye == 1:
-        frame from a single visual fields: origin at the center 2(ecc+buffer_ecc) x 2(ecc+buffer_ecc) (width x height)
-    mask replace all masked pixels with the masked value
+        spatialFrequency: cycle per degree
+        temporalFrequency: Hz
+        direction: 0-2pi in rad
+        phase: 0-2pi in rad
+        a: width of the half image in pixels 
+        b: height of the image in pixels 
+        c1, c2: the two opposite color in rgb values
+        sharpness:  y = A/(1+exp(-sharpness*(x-0.5)) + C, y=x when sharpness = 0
+        buffer_ecc: buffering area, to avoid border problems in texture memory accesses
+        neye == 2:
+            frame from 2 visual fields: (ecc+2*buffer_ecc) x 2(ecc+buffer_ecc+buffer_ecc(unused)) (width x height)
+            each has a temporal axis: [-buffer_ecc, ecc], and vertical axis [-ecc-buffer_ecc, ecc+buffer_ecc] in degree
+        neye == 1:
+            frame from a single visual fields: origin at the center 2(ecc+buffer_ecc) x 2(ecc+buffer_ecc) (width x height)
+        mask replace all masked pixels with the masked value
     """
     if np.mod(npixel,2) != 0:
         raise Exception("need even pixel")
@@ -162,6 +162,8 @@ def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel
         if np.mod(npixel,2) != 0:
             print('failed: npixel need to be even for neye == 2')
             return
+
+    print(f'{npixel} degree per pixel')
 
     b = npixel  
     if genMovie:
@@ -253,13 +255,18 @@ def generate_grating(amp, spatialFrequency, temporalFrequency, direction, npixel
 
     if neye == 1:
         X, Y = np.meshgrid(np.linspace(-1,1,a)*(ecc+buffer_ecc)*np.pi/180, np.linspace(-1,1,b)*(ecc+buffer_ecc)*np.pi/180)
+        deg2pixel = npixel / (2*(ecc+buffer_ecc))
     else:
         X, Y = np.meshgrid((np.linspace(0,1,a)*(ecc+2*buffer_ecc)-buffer_ecc)*np.pi/180,np.linspace(-1,1,b)*(ecc+2*buffer_ecc)*np.pi/180)
+        deg2pixel = npixel / (2*(ecc+2*buffer_ecc))
 
+    print(f'{1/deg2pixel} degree per pixel')
 
     print(f'ecc = {ecc}, buffer_ecc = {buffer_ecc}')
     f = open(fname + '.bin', 'wb')
-    nFrame = int(np.round(frameRate*np.sum(time)))
+    np.array([-1]).astype('i4').tofile(f) 
+    nFrame = np.sum(np.round(np.ceil(frameRate*time))).astype(int)
+    print(nFrame)
     np.array([nFrame, npixel, npixel], dtype='i4').tofile(f)
     mean_value.astype('f4').tofile(f) # init_luminance
     np.array([buffer_ecc, ecc], dtype='f4').tofile(f)
@@ -438,6 +445,205 @@ def generate_circular_mask(npixel, radius, seed, ecc, buffer_ecc, neye, center =
         print(f'{nmasked} ({nmasked/b/a*100:.3f}%) pixel(s) left unmasked')
     return mask
 
+def generate_retinal_wave(amp, spatialFrequency, temporalFrequency, waveSF, waveTF, direction, phase, sharpness, npixel, fname, time, frameRate = 120, ecc = 2.5, gtype = 'drifting', neye = 2, bar = False, center = np.pi/2, wing = np.pi/2, mask = None, maskData = None, genMovie = True, virtual_LGN = 0, nrepeat = 1, reverse = False):
+    if np.mod(npixel,2) != 0:
+        raise Exception("need even pixel")
+    if neye == 1:
+        a = npixel
+    else:
+        a = npixel//2  
+        if neye != 2:
+            print('neye need to be 1 or 2')
+            return
+        if np.mod(npixel,2) != 0:
+            print('failed: npixel need to be even for neye == 2')
+            return
+
+    b = npixel  
+    
+    if waveSF == 0:
+        waveSF = 1/(ecc*2)
+
+    waveSF = waveSF*180/np.pi*2*np.pi
+    waveTF = waveTF*2*np.pi
+
+    if virtual_LGN == 0:
+        nInputType = 2
+    if virtual_LGN == 1:
+        nInputType = 4
+    if virtual_LGN == 2:
+        nInputType = 6
+
+    if genMovie:
+        FourCC = cv.VideoWriter_fourcc(*'HFYU')
+        #FourCC = cv.VideoWriter_fourcc(*'MP4V')
+        output = np.empty(nInputType, dtype = object)
+        for i in range(nInputType):
+            output[i] = cv.VideoWriter(fname+f'_{i}.avi', FourCC, frameRate, (npixel,npixel), True)
+        mixed_output = cv.VideoWriter(fname+'.avi', FourCC, frameRate, (npixel,npixel), True)
+
+    # make sure size of arrays
+    if isinstance(time, (list, tuple, np.ndarray)):
+        nseq = len(time)
+    else:
+        nseq = 1
+        time = np.array([time])
+    amp = check_size(amp, nInputType, nseq, 'amp')
+    spatialFrequency = check_size(spatialFrequency, nInputType, nseq, 'spatialFrequency')
+    temporalFrequency = check_size(temporalFrequency, nInputType, nseq, 'temporalFrequency')
+    direction = check_size(direction, nInputType, nseq, 'direction')
+    phase = check_size(phase, nInputType, nseq, 'phase')
+    sharpness = check_size(sharpness, nInputType, nseq, 'sharpness')
+
+    if mask is not None:
+        if maskData is None:
+            raise Exception('mask data is not provided')
+
+        if len(mask.shape) == 3: 
+            if mask.shape[0] != nseq:
+                raise Exception('nseq mask does not match with time')
+            if mask.shape[1] != b or mask.shape[2] != a:
+                raise Exception('mask frame size does not match')
+        else:
+            if len(mask.shape) != 2:
+                raise Exception('mask array size does not match')
+            else:
+                if mask.shape[0] != b or mask.shape[1] != a:
+                    raise Exception('mask frame size does not match')
+
+        if isinstance(maskData, (list, tuple, np.ndarray)):
+            if isinstance(maskData, (list, tuple)):
+                maskData = np.array(list(maskData))
+
+            if len(maskData.shape) == 3:
+                if maskData.shape[0] != nInputType:
+                    raise Exception(f'maskData takes array of size nInputType = {nInputType} (determined by virtual_LGN) or a single value')
+                else:
+                    if maskData.shape[1] != b or maskData.shape[2] != a:
+                        raise Exception('maskData frame size does not match')
+            else:
+                if len(maskData.shape) != 2:
+                    raise Exception('mask array size does not match')
+                if maskData.shape[1] != b or maskData.shape[2] != a:
+                    raise Exception('maskData frame size does not match')
+                else:
+                    maskData = np.tile(maskData, (nInputType, nseq, 1))
+        else:
+            maskData = np.tile(maskData, (nInputType, nseq, b, a)) 
+            
+
+    ########### VIDEO encodes as BGR: 
+    if neye == 1:
+        X, Y = np.meshgrid(np.linspace(-1,1,a)*ecc*np.pi/180, np.linspace(-1,1,b)*ecc*np.pi/180)
+        deg2pixel = npixel / (2*ecc)
+    else:
+        X, Y = np.meshgrid(np.linspace(0,1,a)*ecc*np.pi/180,np.linspace(-1,1,b)*ecc*np.pi/180)
+        deg2pixel = npixel / (2*ecc)
+
+    print(f'{1/deg2pixel} degree per pixel')
+
+    print(f'ecc = {ecc}')
+    f = open(fname + '.bin', 'wb')
+    np.array([virtual_LGN]).astype('i4').tofile(f) 
+    nFrame = np.sum(np.round(np.ceil(frameRate*time))).astype(int)
+    print(nFrame)
+    np.array([nFrame, npixel, npixel], dtype='i4').tofile(f)
+    np.array([ecc], dtype='f4').tofile(f)
+    np.array([neye]).astype('u4').tofile(f)
+
+    if gtype == 'rotating':
+        half = nstep//2 
+        dl = np.linspace(0,np.pi/4,half)
+        dr = np.linspace(0,np.pi/4,half)
+        dd = np.hstack((dl, np.flip(dr)))
+                
+    dt = 1.0/frameRate
+    #for it in range(1):
+    if gtype not in ('drifting','rotating'):
+        raise Exception(f'gtype {gtype} not implemented')
+
+    if reverse:
+        typeSeq = np.arange(nInputType-1,-1,-1)
+    else:
+        typeSeq = np.arange(nInputType)
+    
+    nFrame = 0
+    for i in range(nseq):
+        t = time[i]
+        nstep = int(np.round(frameRate*t))
+        if not nstep == frameRate*t:
+            nstep = int(np.ceil(frameRate*t))
+            print(f'adjusted to {nstep} frames in total')
+        else:
+            print(f'exact {nstep} frames in total')
+
+        if np.mod(nstep,2) != 0 and gtype == 'rotating':
+            raise Exception(f'need even time step, current: {nstep}')
+
+        for it in range(nstep):
+            t = it * dt
+            bw_seq = np.empty((nInputType, npixel, npixel), dtype=float)
+            for jseq in range(nInputType):
+                j = typeSeq[jseq]
+                radTF = temporalFrequency[j,i]*2*np.pi
+                radSF = spatialFrequency[j,i]*180/np.pi*2*np.pi
+            
+                if neye == 1:
+                    if gtype == 'rotating':
+                        data = retinal_wave(amp[j,i], radTF, radSF, direction[j,i]-dd[it], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+                    if gtype == 'drifting':
+                        data = retinal_wave(amp[j,i], radTF, radSF, direction[j,i], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+
+                    if mask is not None:
+                        data[mask[i,:,:]] = maskData[j,i, mask[i,:,:]]
+                else:
+                    if gtype == 'rotating':
+                        dataL = retinal_wave(amp[j,i], radTF, radSF, direction[j,i]-dd[it], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+                        dataR = retinal_wave(amp[j,i], radTF, radSF, direction[j,i]+dd[it], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+                    if gtype == 'drifting':
+                        dataL = retinal_wave(amp[j,i], radTF, radSF, direction[j,i], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+                        dataR = retinal_wave(amp[j,i], radTF, radSF, direction[j,i], a, b, sharpness[j,i], phase[j,i], t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat)
+
+                    if mask is not None:
+                        assert(dataL.shape[0] == b)
+                        assert(dataL.shape[1] == a)
+                        dataL[mask[i,:,:]] = maskData[j,i, mask[i,:,:]]
+                        dataR[mask[i,:,:]] = maskData[j,i, mask[i,:,:]]
+                    data = np.concatenate((dataL,dataR), axis = 1)
+
+                #if i == 0 and it == 0 and j == 0:
+                #    print(data)
+
+                bw = np.repeat(data, 3).reshape(npixel,npixel,3)
+                pixelData = np.round(bw*255).astype('uint8')
+                bw_seq[jseq,:,:] = data
+                if genMovie:
+                    output[jseq].write(pixelData)
+                #pixelData = np.reshape(np.round(data*255), (b,a,3))
+                #cv.imshow('linear', pixelData)
+                #cv.waitKey(0)
+                #pixelData = adjust_gamma(pixelData, gamma = 2.2)
+                #cv.imshow('gamma', pixelData)
+                #cv.waitKey(0)
+            bw_seq.astype('f4').tofile(f)
+            nFrame = nFrame + 1
+
+            if genMovie:
+                mixed_data = (bw_seq[0,:,:] - bw_seq[1,:,:] + 1)/2
+                pixelData = np.round(np.repeat(mixed_data, 3).reshape(npixel,npixel,3)*255).astype('uint8')
+                mixed_output.write(pixelData)
+            
+
+    f.close()
+    print(f'{nFrame} in total')
+
+    if genMovie:
+        for j in range(nInputType):
+            output[j].release()
+        mixed_output.release()
+        cv.destroyAllWindows()
+    return
+
 def adjust_gamma(image, gamma=1.0):
     # build a lookup table mapping the pixel values [0, 255] to
     # their adjusted gamma values
@@ -467,6 +673,22 @@ def logistic(sharpness):
             return x
         return static_nolinearity
     return decorator_logistic
+
+def check_size(target, first, second, label = 'some var'):
+    if isinstance(target, (list, tuple, np.ndarray)):
+        if not (len(target) == first or len(target) == second):
+            raise Exception(f'{label} does not match') 
+        else:
+            if len(target) == first:
+                target = np.tile(np.array([target]), (second,1)).T
+            else:
+                target = np.tile(np.array([target]), (first,1))
+    else:
+        target = np.tile(np.array([target]), (first, second))
+
+    assert(target.shape[0] == first)
+    assert(target.shape[1] == second)
+    return target
 
 def sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, control, sharpness, phase, t, X, Y, bar, center, wing):
     phi = (np.cos(direction)*X + np.sin(direction)*Y)*radSF - radTF*t
@@ -498,6 +720,36 @@ def sine_wave(amp, radTF, radSF, direction, a, b, c1, c2, control, sharpness, ph
             assert((color[:,i] <= c2[0,i]).all())
         
     return color.reshape((b,a,3))
+
+def retinal_wave(amp, radTF, radSF, direction, a, b, sharpness, phase, t, X, Y, bar, center, wing, waveSF, waveTF, nrepeat):
+    wave_ratio = waveSF/radSF # period/wave_period
+    # phase is alreayd wave phase
+    phi0 = (np.cos(direction)*X + np.sin(direction)*Y)*waveSF - waveTF*t - phase
+    wing0 = wing*wave_ratio
+    center0 = np.pi - wing0
+    pick = np.zeros(X.shape, dtype = bool)
+    phi = np.zeros(X.shape) + center + wing
+    for i in range(nrepeat):
+        tmp_pick = np.abs((phi0 + i*2*np.pi) - center0) < wing0
+        phi[tmp_pick] = (phi0[tmp_pick] + i*2*np.pi - center0 + wing0) / wave_ratio - wing + center
+
+    bw = np.reshape((np.sin(phi) - np.sin(wing+center))/(1-np.sin(wing+center))*amp, (a*b,1))
+    assert((bw >= 0).all())
+    assert((bw <= 1).all())
+    if sharpness != 1:
+        if sharpness > 0:
+            exp_half = np.exp(sharpness/2)+1
+            A = np.power(exp_half,2)/(np.exp(sharpness)-1)
+            C = exp_half/(1-np.exp(sharpness))
+            bw = A/(1.0 + np.exp(-sharpness*(bw-0.5))) + C
+        else:
+            bw[bw > 5e-16] = 1
+
+    assert((bw <= 1.0+5e-16).all())
+    assert((bw >= 0.0-5e-16).all())
+    bw[bw > 1] = 1
+    bw[bw < 0] = 0
+    return bw.reshape((b,a))
 
 def randomized(amp, a, b, c0, gtype):
     if gtype == 'uniform':
