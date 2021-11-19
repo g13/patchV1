8ca97952202da73af459ab4c73f4490497f2451c
Wei Dai <weidai@fudan.edu.cn>
2021-11-19 11:41:43 +0800

    relay

--- patch_square.py
+++ patch_square.py
@@ -8,3 +8,9 @@ def square_pos(per_dis, w, h, center):
     pos[0,:] = (w*rands[:,0]-w/2)*per_dis[0] + center[0]
     pos[1,:] = (h*rands[:,1]-h/2)*per_dis[1] + center[1]
     return pos
+
+def square_OP(center, r, phase, pos):
+    dis = pos - center
+    op = np.arctan2(dis) + phase
+    pick = (pos-center).all(1) < r
+    return op, pick
