6743beb55a6e132a0afb77dbe2edda335c8b6077
Wei Dai <weidai@fudan.edu.cn>
2021-11-28 18:44:40 +0800

    square layer

--- patch_square.py
+++ patch_square.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.stats import qmc
-
+import warnings
+from sys import stdout
+#warnings.filterwarnings("once", message = "The balance properties of Sobol' points require")
+warnings.filterwarnings("ignore", message = "The balance properties of Sobol\' points require", category=UserWarning)
 def square_pos(per_dis, n, center):
     sampler = qmc.Sobol(d=2)
     rands = sampler.random(n)
@@ -23,3 +26,43 @@ def square_OP(center, r, phase, pos, clockwise):
     op[dpick2] = op[dpick2] + np.pi
     pick = (np.abs(dis) < r).all(1)
     return op, pick
+
+
+def assign_square_xyID(n, pos, ratio = 2):
+    xyID = np.zeros((2,n), dtype = 'i4')
+    w = int(np.sqrt(n))
+    xy = np.zeros((2,w*w), dtype = 'i4')
+    xy[0,:] = np.tile(np.arange(w), w)
+    xy[1,:] = np.repeat(np.arange(w), w)
+    nonreplace_pick = np.ones(w*w, dtype = bool)
+    pos_id = np.arange(w*w)
+    pos = pos*w
+    xy = xy.T
+    max_dis = 1
+    #max_dis = 2*w*w
+    for i in range(n):
+        dis = np.max(np.abs(pos[:,i] - xy), axis = 1)
+        # local algo
+        local_pick = dis <= max_dis
+        #count = 0
+        # slow down x1000!!!
+        #while sum(local_pick) == 0: 
+        #    max_dis *= ratio
+        #    local_pick = np.logical_and(nonreplace_pick,dis <= max_dis)
+        #    count += 1
+        local_dis = dis[local_pick]
+        local_id = pos_id[local_pick]
+        imin = np.argmin(local_dis)
+        xyID[:,i] = xy[local_id[imin],:]
+        nonreplace_pick[local_id[imin]] = False 
+        #if count > 0:
+        #    print(count, sum(local_pick))
+
+        # global algo
+        #dis[np.logical_not(nonreplace_pick)] = max_dis
+        #imin = np.argmin(dis)
+        #xyID[:,i] = xy[imin,:]
+        #nonreplace_pick[imin] = False
+        stdout.write(f'\r {i/n*100:.1f}%')
+    stdout.write('\n')
+    return xyID, w
