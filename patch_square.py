b0a1a6827fdb611f9be487498b230045da7a4a66
Wei Dai <gueux13@gmail.com>
2022-04-26 00:31:16 +0800

    single layer setup update

--- patch_square.py
+++ patch_square.py
@@ -12,18 +12,19 @@ def square_pos(per_dis, n, center):
     pos[1,:] = (rands[:,1]-1/2)*per_dis[1] + center[1]
     return pos
 
-def square_OP(center, r, phase, pos, clockwise):
+def square_OP(center, r, phase0, phase, pos, clockwise):
     dis = pos.T - center
     op = np.arctan2(dis[:,1], dis[:,0])/2 
+    op = op + phase/2
     if clockwise:
-        op = (np.pi - (op + np.pi/2)) - np.pi/2
-        op = op - phase
-    else:
-        op = op + phase
-    dpick1 = op > np.pi/2
-    dpick2 = op < -np.pi/2
-    op[dpick1] = op[dpick1] - np.pi
-    op[dpick2] = op[dpick2] + np.pi
+        op = -op
+    op = op + phase0/2
+
+    while (np.abs(op) >= np.pi/2).any():
+        dpick1 = op > np.pi/2
+        dpick2 = op < -np.pi/2
+        op[dpick1] = op[dpick1] - np.pi
+        op[dpick2] = op[dpick2] + np.pi
     pick = (np.abs(dis) < r).all(1)
     return op, pick
 
